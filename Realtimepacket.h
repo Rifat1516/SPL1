524bd090b44cbb0f77f9960da571f735f57e79cd
Asaduzzaman Rifat <150079697+Rifat1516@users.noreply.github.com>
2026-01-15 04:03:52 +0600

    Remove obsolete function prototypes from Realtimepacket.h
    Removed function prototypes that rely on Mac-compatible headers.

--- Realtimepacket.h
+++ Realtimepacket.h
@@ -1,16 +1,12 @@
+#ifndef PACKET_HEADER_H
+#define PACKET_HEADER_H
+
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <netinet/ip_icmp.h>
 
-/* These prototypes remain the same, but now they rely on Mac-compatible headers */
-void IPheader(unsigned char*, int);
-void tcpPacket(unsigned char*, int);
-void udpPacket(unsigned char*, int);
-void icmpPacket(unsigned char*, int);
-void Hexdata(unsigned char*, int);
-int sslPacket(unsigned char*, int);
-void CapturingPacket(unsigned char*, int);
-int Realtimepacket(int x); // Added 'int x' to match your recent implementation
+#endif
