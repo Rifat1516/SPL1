22855797318cb0a2889d248e6778f1f34d41b600
Asaduzzaman Rifat <150079697+Rifat1516@users.noreply.github.com>
2026-04-22 10:41:16 +0600

    Add files via upload

---
+++ icmpRead.h
@@ -0,0 +1,5 @@
+void ICMPinfo(unsigned char *data,int size);
+void IPtype(unsigned char *type);
+void packettype(unsigned char *type);
+void protocoltype(unsigned char *type);
+void hardwaretype(unsigned char *type);
