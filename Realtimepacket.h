4fa31371f65356ebc31785a4a8e0f6b3edf6ea47
Asaduzzaman Rifat <150079697+Rifat1516@users.noreply.github.com>
2026-04-22 10:34:56 +0600

    Refactor header guards and remove unused includes

--- Realtimepacket.h
+++ Realtimepacket.h
@@ -1,12 +1,6 @@
-#ifndef PACKET_HEADER_H
-#define PACKET_HEADER_H
+#ifndef REALTIMEPACKET_H
+#define REALTIMEPACKET_H
 
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
-#include <netinet/udp.h>
-#include <netinet/ip_icmp.h>
+int Realtimepacket(char *target, int x);
 
 #endif
